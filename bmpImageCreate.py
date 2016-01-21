adb3bdba3b537fa228d7a35810b23422f9cd1b06
levitan <levitan@msn.cn>
2016-01-21 15:18:05 +0800

    add funSinLine and bmpImageCreateWB8Bit

--- bmpImageCreate.py
+++ bmpImageCreate.py
@@ -1,6 +1,7 @@
 __author__ = 'levitan'
 
 from PIL import Image
+import numpy as np
 from numpy import *
 
 
@@ -14,7 +15,17 @@ def bmpImageCreateRGB(x, y, name, function):
     c.save("%s.bmp" % name)
 
 
-def bmpImageCreateWB(x, y, name, funtion):
+def bmpImageCreateWB8Bit(x, y, name, function):
+    c = Image.new("L", (x, y))
+    meshArray = function(x, y)
+    for i in range(0, x):
+        for j in range(0, y):
+            c.putpixel([i, j], int(meshArray[i][j]))
+    # c.show()
+    c.save("%s.bmp" % name)
+
+
+def bmpImageCreateWB1Bit(x, y, name, funtion):
     c = Image.new('1', (x, y))
     meshArray = funtion(x, y)
     for i in range(x):
@@ -29,12 +40,12 @@ def bmpImageCreateWB(x, y, name, funtion):
 
 def funLine(x, y):
     gridWidth = int(x / 50)
-    meshAarry = zeros((x, y))
+    meshArray = zeros((x, y))
     width = 0
     Black = True
     for i in range(x):
         if Black:
-            meshAarry[i][:] = 255
+            meshArray[i][:] = 255
             if width == gridWidth:
                 width = 0
                 Black = not Black
@@ -42,11 +53,35 @@ def funLine(x, y):
             width = 0
             Black = not Black
         width += 1
-    return meshAarry
+    return meshArray
+
+
+def funSinLine(x, y):
+    gridWidth = int(x / 20)
+    meshArray = zeros((x, y))
+    for i in range(x):
+        meshArray[i, :] = abs(sin(pi * i / gridWidth) ** 2 * 255)
+    return meshArray
+
+
+def funLaguerre(x, y):
+    meshArray = zeros((x, y))
+    c = [1, 1, 1, 1, 1]
+    max = abs(np.polynomial.laguerre.lagval2d(x, y, c))
+    for i in range(x):
+        for j in range(y):
+            meshArray[i, j] = abs(np.polynomial.laguerre.lagval2d(i, j, c)) * 255 / max
+            # print meshArray[i,j]
+            if meshArray[i, j] > 255:
+                meshArray[i, j] = 255
+    return meshArray
 
 
 def funCicle(x, y):
-    cicleR = int(x / 4)
+    if x < y:
+        cicleR = int(x / 4)
+    else:
+        cicleR = int(y / 4)
     meshArray = zeros((x, y))
     for i in range(x):
         for j in range(y):
@@ -64,6 +99,6 @@ def funRandom(x, y):
 
 
 def ImageTest():
-    #bmpImageCreateRGB(912, 1140, 'Random', funRandom)
-    bmpImageCreateWB(912,1140,'WBCicle',funCicle)
-    bmpImageCreateWB(912,1140,'WBLine',funLine)
+    # bmpImageCreateRGB(912, 1140, 'Laguerre', funLaguerre)
+    bmpImageCreateWB8Bit(912, 1140, 'WB8BitSinLine', funSinLine)
+    # bmpImageCreateWB1Bit(912, 1140, 'WBLine', funLine)
